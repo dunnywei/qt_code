546cf22291990bdb022008c877bb6979cd6c0afa
wei_don@yahoo.com <wei_don@yahoo.com>
2020-01-16 21:08:42 -0500

    adding all

--- test3/main.cpp
+++ test3/main.cpp
@@ -4,11 +4,18 @@ int main(int argc,char* argv[])
 {
     QApplication app(argc,argv);
 
-
+   /*
 	QLabel *label=new QLabel("hellow world");
 	label->setWindowTitle("Mine");
 	label->resize(400,400);
 	label->show();
+   */
+
+
+	QLabel label("hellow world");
+	label.setWindowTitle("Mine");
+	label.resize(400,400);
+	label.show();
 
     return app.exec();
 }
